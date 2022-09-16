985cf01dce888d8e7681f2810db7d51bc87bc567
samuel-ma <mabilmakur@gmail.comm>
2022-09-16 16:15:45 +0200

    Add project files.

---
+++ 1000 Problems/1000 Problems.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Enter any two numbers: " << endl;
+	cin >> a >> b;
+	
+	for (int i = a; i <= b; i++) {
+		if(i>=1 && i<=9){
+			if (i == 1) cout << "one" << endl;
+			if (i == 2) cout << "two" << endl;
+			if (i == 3) cout << "three" << endl;
+			if (i == 4) cout << "four" << endl;
+			if (i == 5) cout << "five" << endl;
+			if (i == 6) cout << "six" << endl;
+			if (i == 7) cout << "seven" << endl;
+			if (i == 8) cout << "eight" << endl;
+			if (i == 9) cout << "nine" << endl;
+		}
+		else if (i > 9 && i % 2 == 0) {
+			cout << "even" << endl;
+		}
+		else {
+			cout << "odd" << endl;
+		}
+	}
+
+}
