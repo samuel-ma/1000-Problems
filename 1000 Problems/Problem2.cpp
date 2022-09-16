d05c791d8958f3b481ba86d884e98afff6426b25
samuel-ma <mabilmakur@gmail.comm>
2022-09-16 17:02:19 +0200

    problem 2 commit

--- 1000 Problems/Problem2.cpp
+++ 1000 Problems/Problem2.cpp
@@ -1,14 +1,42 @@
-
 //using Functions
+
+/*
+* QUESTION 2
+* Max of four
+* 
+* Write a function int max_of_four(int a, int b, int c, int d)
+* which returns the maximum of the four arguments it receives.
+* 
+* Input Format
+* Input will contain four integers -a, b, c , d one per line
+* 
+* Output Format
+* Return the greatest of the four integers
+* PS: I/O will be automatcially handled
+* 
+* Sample Input
+* 2
+* 3
+* 6
+* 5
+* 
+* Sample Output
+* 6
+*/
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-void newFunction() {
+int sum_of_numbers() {
 
+	int a, b, c, d;
+	cout << "Enter any four numbers: " << endl;
+	cin >> a >> b >> c >> d;
+	
+	
 }
 
 int main() {
-	newFunction();
+	sum_of_numbers();
 }
