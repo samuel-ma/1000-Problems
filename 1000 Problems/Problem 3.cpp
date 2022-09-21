179130a25785fb3a2b56b27398a11c0e09ef9cae
Samuel <82509653+samuel-ma@users.noreply.github.com>
2022-09-21 21:36:55 +0200

    Problem 3
    Size of a Vector

---
+++ 1000 Problems/Problem 3.cpp
@@ -0,0 +1,18 @@
+//PROBLEM 3
+//Find the size of a vector using the size() function
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> arr1 = { 1, 2, 3, 4 };
+    vector <int> arr2 = {};
+    vector <float> arr3 = { 1.2, 3.8, 3.0, 2.7, 6.6 };
+
+    cout << "Size of arr1: " << arr1.size() << endl;
+    cout << "Size of arr2: " << arr2.size() << endl;
+    cout << "Size of arr3: " << arr3.size() << endl;
+
+    return 0;
+}
